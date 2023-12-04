bfe2ea06cb960c1cdaab2bc89e9d61a94bffd1b9
Michael Mahlberg <mm@consulting-guild.de>
2023-12-04 02:15:06 +0100

    feat: we can solce the power game of the elves

--- Day2/ruby/AoCDay2/aoc_day2.rb
+++ Day2/ruby/AoCDay2/aoc_day2.rb
@@ -52,10 +52,18 @@ end
 def accumulate_game_numbers(the_input)
   result = 0
   the_input.each_line do |line|
-    break if line == nil
     result = evaluate_line( line, result)
   end
   return result
+end
+
+
+def sum_power_for_multiple_lines(the_lines)
+  the_sum = 0
+  the_lines.each_line do |one_line|
+    the_sum += get_the_power(one_line)
+  end
+  return the_sum
 end
 
 class ItRejectsImpossibleGames < Minitest::Test
@@ -121,9 +129,50 @@ class ItRejectsImpossibleGames < Minitest::Test
 
 end
 
-final_result = accumulate_game_numbers(File.open("Real-DiceData-d2p1.txt") )
+def get_the_power( elven_input)
+  game_number, dice_info = getInfoParts(elven_input)
+  return 0 if game_number.nil? or dice_info.nil?
 
-puts "Final result «#{final_result}»"
+  color_hash = {"red" => 0, "blue" => 0, "green" => 0}
+
+  dice_info.split(";").each do |draw|
+    split_draw(draw).each do |dice_set|
+      count, color = split_dice_set(dice_set)
+      if color_hash[color] < count
+        color_hash[color] = count
+      end
+    end
+  end
+  color_hash["red"] * color_hash["blue"] * color_hash["green"]
+end
+
+class ItCountsPowersPerLine < Minitest::Test
+  PowersPerLine=[48,12,1560,630,36]
+  SummedUpPower=2286
+
+  def test_power_per_line_add_upp
+    assert_equal( SummedUpPower, PowersPerLine.sum )
+  end
+
+  def test_power_game_three
+    assert_equal( PowersPerLine[0], get_the_power("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"))
+  end
+  def test_power_game_one
+    assert_equal( PowersPerLine[2], get_the_power("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"))
+  end
+
+  def test_counting_power_for_multiple_lines
+    assert_equal(SummedUpPower, sum_power_for_multiple_lines( Dice_data_as_string ))
+  end
+end
+
+final_result_day_2_part_1 = accumulate_game_numbers(File.open("Real-DiceData-d2p1.txt") )
+
+puts "Final result «#{final_result_day_2_part_1}»"
+
+final_result_day_2_part_2 = sum_power_for_multiple_lines(File.open("Real-DiceData-d2p1.txt") )
+
+puts "Final result «#{final_result_day_2_part_2}»"
 
 
 
